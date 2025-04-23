66c6100ea60535edcbc5fcf6276a7d02aea98fc3
leah <l0583280864@gmail>
2025-04-23 10:39:31 +0300

    hope finish

--- src/service.js
+++ src/service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = process.env.REACT_APP_URL;
+axios.defaults.baseURL =process.env.REACT_APP_URL;
 axios.interceptors.response.use(
   response=>response,
   err=>{
@@ -10,13 +10,15 @@ axios.interceptors.response.use(
 )
 export default {
   getTasks: async () => {
-    const result = await axios.get(``)    
-    return result?.data;
+    const result = await axios.get('')    
+    return result.data;
   },
 
   addTask: async(name)=>{
-    const result = await axios.post(``,{name,isComplete:false})    
-    return result?.data;
+    console.log(name);
+    
+    const result = await axios.post(`?name=${name}`)    
+    return result.data;
   },
 
   setCompleted: async(id, isComplete)=>{
